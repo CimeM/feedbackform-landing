3fa65c50237c411c19dc80426a639e82ca1bcf1b
masher <cim.martin@gmail.com>
2026-09-20 15:32:26 +0200

    fix: 404

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -7,6 +7,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://lerins.rivieraapps.com",
+  trailingSlash: "always",
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
